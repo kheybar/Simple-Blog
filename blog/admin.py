cb2a2c34f6738a8a7851babeb9eab33acc1585ba
Mahdi <MahdiZarepour15@gmail.com>
2021-10-11 17:22:37 +0330

    model Article for app blog create.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'writer', 'slug']
+
+admin.site.register(Article, ArticleAdmin)
--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Our Application
-    'blog.app.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
