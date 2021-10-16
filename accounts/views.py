339c77c3411dc6dd8cac3d54156efebdd95bc4ea
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-16 09:53:55 +0330

    add message for login system.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from .forms import UserLoginForm
 
 
@@ -13,8 +13,10 @@ def user_login(request):
             user = authenticate(request, username=username, password=password) # if user not found in DB, return None
             if user is not None:
                 login(request, user) # this method => set user to request and use in MVT
+                messages.success(request=request, message='you logged in successfully.', extra_tags='success') # befor redirect
                 return redirect('blog:all_articles')
-
+            else:
+                messages.error(request=request, message='wrong username or password', extra_tags='warning') # extra_tags: for send anythings with message like bootstrap classes
     else:
         form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
