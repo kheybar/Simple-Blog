bb1913d2da255215830685e4999a6acd86edd1d1
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-16 10:17:50 +0330

    update login form.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 
-
 class UserLoginForm(forms.Form):
     username = forms.CharField(
         max_length=50, 
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'your username'})
     )
     password = forms.CharField(
         max_length=50,
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'your password'})
     )
