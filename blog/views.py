462ad951a9f0218f1e22045039a8a3bc2ed7aefb
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-18 10:34:29 +0330

    git vs filter.

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,24 @@ def all_articles(request):
 def article_detail(request, id, slug):
     article = get_object_or_404(Article, id=id, slug=slug)
     return render(request, 'blog/article_detail.html', {'article': article})
+
+
+
+# filter or get?
+# def article_detail(request, id, slug):
+
+    # گت مستقیم میاد اطلاعات رو میریزه داخل متغییر. پس براحتی قابل دسترس هستند
+    # گت برای زمانی هست که بخوایم یک آیتم رو از دیتابیس بیاریم و اگر شد چندتا، بهمون ارور میده
+    # article = Article.objects.get(id=id, slug=slug)
+    # print('*' * 90)
+    # print(article) # install Manjaro
+
+
+    # فیلتر اطلاعات رو میریزه داخل یک لیست و بعد به متغییر میده. پس برای اینکه در تمپلیت بهش دسترسی داشته باشیم باید روش حلقه ی فور بزنیم
+    # چون لیست هست به راحتی میتونیم چند ایتم رو از دیتابیس بیاریم
+    # article = Article.objects.filter(id=id, slug=slug)
+    # print('*' * 90)
+    # print(article) # <QuerySet [<Article: install Manjaro>]>
+
+
+    # return render(request, 'blog/article_detail.html', {'article': article})
